35e62137ff7d5da6e76ac6266b0070e19558df0a
David Kajpust <kajpustd@gmail.com>
2019-03-25 16:38:06 -0400

    Added token into manifest, added more scaffolding files, added token abi, notes on PLCRvoting.

---
+++ src/mappings/PLCRVoting.ts
@@ -0,0 +1,13 @@
+/*
+  it seems like this just just votes for tokens
+  and that the events emitted by it help
+  they never delete data
+  probably the registry calls into here, checks isPassed
+
+  we would wanna check is passed
+
+  it is debateable whether or not we need POLLs , the registry might cover it
+  this might be all extra info
+
+  that i can decide if i want to add
+ */
